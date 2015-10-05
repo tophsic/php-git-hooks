b115a42660cad7f0c649ffba18a30caa5370a1ab
Christophe Sicard <sicard.christophe@gmail.com>
2015-10-05 07:05:47 +0200

    Fix require bootsrap in local installation

--- hooks/bootstrap.php
+++ hooks/bootstrap.php
@@ -10,16 +10,13 @@ if (false !== getenv('COMPOSER_HOME')) {
     $home = getenv('COMPOSER_HOME');
 }
 
-$autoload = __DIR__ . '/../../vendor/autoload.php';
-
 if (
     false !== $home &&
     0 !== preg_match('~' . $home . '~', __DIR__)
 ) {
     $binDir = $home . '/vendor/bin';
-    $autoload = __DIR__ . '/../../../../vendor/autoload.php';
 }
 
 define('PHPGITHOOKS_BIN_DIR', $binDir);
 
-require $autoload;
+require __DIR__ . '/../../../../vendor/autoload.php';
